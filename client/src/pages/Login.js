5202f26da0dd3f21536113c5d82cb5f53adc15e6
Peter Jolley <peterjolley456@gmail.com>
2022-08-16 19:44:48 +0930

    Login and Signup changed to tailwind Header code changed slightly

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -89,7 +89,7 @@ const Login = (props) => {
               </form>
       )}
        {error && (
-        <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+        <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
         
      
           {error.message}
