331cb3098edef3c24223abc236d5ec81643755c4
louieiply <louieiply@gmail.com>
2022-08-17 23:05:52 +0930

    Added products page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -86,6 +86,7 @@ const Header = () => {
                 to="/products"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
+
                 Products
               </Link>
             </li>
@@ -113,6 +114,7 @@ const Header = () => {
             ) : (
               <>
                 <li>
+                  
                   <Link
                     to="/login"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
