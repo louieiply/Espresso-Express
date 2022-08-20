a0a4bd40813c1fa03e02f5dde6719a806a30d45f
Peter Jolley <peterjolley456@gmail.com>
2022-08-20 13:22:38 +0930

    Milk seeds added and code written

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,6 +2,7 @@ const db = require('../config/connection');
 const { User, Product, Category } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const categorySeeds = require('./categorySeeds.json');
+const milkSeeds = require('./milkSeeds.json')
 const productSeeds = require('./productSeeds.json');
 
 db.once('open', async () => {
@@ -22,8 +23,19 @@ db.once('open', async () => {
       tempProduct.categoryId = categoryId;
       newProductSeeds[index] = tempProduct;
     }
-    
+
     await Product.create(newProductSeeds);
+
+    const milkcateId = response[0]._id.toString();
+    var newMilkProductSeeds = milkSeeds;
+    for (let index = 0; index < newMilkProductSeeds.length; index++) {
+      var tempProduct = newMilkProductSeeds[index];
+      tempProduct.categoryId = milkcateId;
+      newMilkProductSeeds[index] = tempProduct;
+    }
+    
+    await Product.create(newMilkProductSeeds);
+    
   } catch (err) {
     console.error(err);
     process.exit(1);
