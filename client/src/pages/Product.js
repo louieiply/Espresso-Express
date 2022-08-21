d5ec72b4b254e0947e0b4514b53ddd4f9d3d6678
Peter Jolley <peterjolley456@gmail.com>
2022-08-22 01:14:01 +0930

    Product changed and updated

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -46,13 +46,7 @@ const Product = () => {
                 </div>
             </div>
 
-            <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
-                <div>
-                    <a title="Buy me a beer" href="https://www.buymeacoffee.com/scottwindon" target="_blank" className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
-                        <img className="object-cover object-center w-full h-full rounded-full" src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg"/>
-                    </a>
-                </div>
-            </div>
+           
         </div>   
     )
 }
