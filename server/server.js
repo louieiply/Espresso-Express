45c72db248bb326d183dd62f5c4f5ef4f6445129
louieiply <louieiply@gmail.com>
2022-08-22 17:17:27 +0930

    Update successful

--- server/server.js
+++ server/server.js
@@ -17,11 +17,13 @@ const server = new ApolloServer({
   playground: true,
 });
 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')) );
 }
 
 app.get('/', (req, res) => {
