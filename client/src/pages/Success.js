389ef1b3c6e807e27dc3a5f0571923019a3f8677
Peter Jolley <peterjolley456@gmail.com>
2022-08-22 17:35:28 +0930

    success file writen home link working

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
+import { Link } from "react-router-dom";
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
@@ -20,9 +21,9 @@ function Success() {
         });
       }
 
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
+      // setTimeout(() => {
+      //   window.location.assign(window.location.origin);
+      // }, 4000);
     }
 
     saveOrder();
@@ -30,9 +31,28 @@ function Success() {
 
   return (
     <div>
-        <h1>Success!</h1>
+        {/* <h1>Success!</h1>
         <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
+        <h2>You will now be redirected to the home page</h2> */}
+        <div class="bg-gray-100 h-screen">
+      <div class="bg-white p-6  md:mx-auto">
+        <svg viewBox="0 0 24 24" class="text-green-600 w-16 h-16 mx-auto my-6">
+            <path fill="currentColor"
+                d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z">
+            </path>
+        </svg>
+        <div class="text-center">
+            <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">Success!</h3>
+            <p class="text-gray-600 my-2">Thank you for your secure online purchase.</p>
+            <p> Have a great day!  </p>
+            <div class="py-10 text-center">
+                <Link to ="/" class="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3">
+                    GO BACK 
+               </Link>
+            </div>
+        </div>
+    </div>
+  </div>
     </div>
   );
 }
